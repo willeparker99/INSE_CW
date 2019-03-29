ee000dfe44b93e58a8738c14d2a063ad1563efa5
charlie-s1 <charlie.d.smith98@gmail.com>
2019-03-29 16:28:43 +0000

    organise server and static

--- server/server.js
+++ server/server.js
@@ -4,10 +4,4 @@ const app = express();
 
 app.use(express.static('static'));
 
-app.use("/js", express.static('js'));
-
-app.use("/img", express.static('img'));
-
-app.use("/", function(req, res){
-})
 app.listen(8080);
