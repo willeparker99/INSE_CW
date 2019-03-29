0152b48aa72fdf4f7c4257d6e83c0fcc4826c04d
AthosCosta <athos_costa@users.noreply.github.com>
2019-03-29 21:19:38 +0000

    Notifications update

--- static/js/main.js
+++ static/js/main.js
@@ -75,25 +75,25 @@ function search(e){
     }
   }
 }
-
-function notifyMe() {
-  // Let's check if the browser supports notifications
-  if (!("Notification" in window)) {
-    alert("This browser does not support desktop notification");
-  }
-
-  // Let's check whether notification permissions have already been granted
-  else if (Notification.permission === "granted") {
-    // If it's okay let's create a notification
-    var notification = new Notification("Hi there!");
-  }
-
-  // Otherwise, we need to ask the user for permission
-  else if (Notification.permission !== "denied") {
-    Notification.requestPermission().then(function (permission) {
-      // If the user accepts, let's create a notification
-      if (permission === "granted") {
-        var notification = new Notification("Hi there!");
-      }
-    });
-  }
+// 
+// function notifyMe() {
+//   // Let's check if the browser supports notifications
+//   if (!("Notification" in window)) {
+//     alert("This browser does not support desktop notification");
+//   }
+//
+//   // Let's check whether notification permissions have already been granted
+//   else if (Notification.permission === "granted") {
+//     // If it's okay let's create a notification
+//     var notification = new Notification("Hi there!");
+//   }
+//
+//   // Otherwise, we need to ask the user for permission
+//   else if (Notification.permission !== "denied") {
+//     Notification.requestPermission().then(function (permission) {
+//       // If the user accepts, let's create a notification
+//       if (permission === "granted") {
+//         var notification = new Notification("Hi there!");
+//       }
+//     });
+//   }
