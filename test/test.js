1d9361e99b3a061e130a1827d3042119df068f82
willeparker99 <willeparker99@gmail.com>
2019-04-04 16:36:34 +0100

    Test case fix

---
+++ test/test.js
@@ -0,0 +1,110 @@
+"use strict";
+const fs = require("fs");
+const path = require("path");
+const QUnit = require("qunit");
+
+const serverFile = path.join("../server/sever.js");
+
+QUnit.module("Function Checking");
+
+QUnit.test("Has everything compiled correctly",
+    function(assert){
+        assert.ok(
+            //check functions inside main.js
+            typeof createDeals === "function",
+            "check if createDeals is a working function."
+        );
+        assert.ok(
+            typeof init === "function",
+            "check if init is a working function."
+        );
+        assert.ok(
+            typeof init === "function",
+            "check if init is a working function."
+        );
+        assert.ok(
+            typeof shareURL === "function",
+            "check if shareURL is a working function."
+        );
+        assert.ok(
+            typeof serach === "function",
+            "check if search is a working function."
+        );
+        //check functions inside nav.js
+        assert.ok(
+            typeof serach === "function",
+            "check if search is a working function."
+        );
+    }
+);
+QUnit.test(
+    'Does the `server.js` file exists',
+    (assert) => {
+        try{
+            fs.accessSync(serverFile, fs.F_OK);
+            assert.pushResult({result: true, message: "`server.js` found"});
+        } catch (e) {
+            assert.pushResult({result: failed, message: "`server.js` not found."});
+        }
+    }
+);
+QUnit.test(
+    "Start server.",
+    async (assert) => {
+        const done = assert.async();
+
+        try{
+            const serverBASICCHECK = await fetch("/", {
+                method: 'GET',
+            });
+            assert.deepEqual(
+                serverBASICCHECK.status,
+                200,
+                'The server has successfully been created'
+            );
+            assert.async();
+        } catch (e){
+            console.log("Did not recieve message from server: " + e);
+        }
+    }
+);
+QUnit.test(
+    "Check for index page.",
+    async (assert) => {
+        const done = assert.async();
+
+        try {
+            const serverBASICCHECK = await fetch("/index.html", {
+                method: 'GET',
+            });
+            assert.deepEqual(
+                serverBASICCHECK.status,
+                200,
+                '`index.html` has been found'
+            );
+            assert.async();
+        } catch (e) {
+            console.log("Did not recieve message from server: " + e);
+        }
+    }
+);
+QUnit.test(
+    "Check for home page.",
+    async (assert) => {
+        const done = assert.async();
+
+        try {
+            const serverBASICCHECK = await fetch("/index.html", {
+                method: 'GET',
+            });
+            assert.deepEqual(
+                serverBASICCHECK.status,
+                200,
+                '`home.html` has been found.'
+            );
+            assert.async();
+        } catch (e) {
+            console.log("Did not recieve message from server: " + e);
+        }
+    }
+);
